c0233169e8e97fcf73a529320e4f37b45d313218
Natalie <nataliebock19@gmail.com>
2025-02-09 13:06:14 -0500

    Update main.py
    Adding comments to explain key sections in the code

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -4,22 +4,24 @@ import pandas as pd
 #Instructions: streamlit run basic_streamlit_app/main.py 
 
 penguins = pd.read_csv("Data/penguins.csv")
-
+##Adding title and explaining the app 
 st.title("Penguins App")
 st.text("- This app uses a dataset with information about penguins.")
 st.text("- You can look at penguins based on species, island, and other facts!")
 st.text("- In order to view information, click on the drop down menus and sliders.")
 
+#A simply button encourage excitement about penguins! 
+
 if st.button("Click if you are excited about penguins!"):
     st.write("Yay you clicked let's learn!")
 else:
     st.write("You don't want to learn about penguins :(")
-
+# Now the interactive widgets inclduing drop down/ select box and sliders 
 st.subheader("Now, let's look at some penguins!")
-
+#Full table:
 st.write("Here's the full table:")
 st.dataframe(penguins)
-
+#by species and then island: 
 species = st.selectbox("Select a species", penguins["species"].unique()) 
 
                       
@@ -35,7 +37,7 @@ filtered_pen2 = penguins[penguins["island"] == island]
 st.write(f"Penguins that are on {island}:") 
 st.dataframe(filtered_pen2)
 
-
+#Sliders for mass, flipper and bill length 
 mass = st.slider("Choose a body mass range in grams:",
                    min_value = penguins["body_mass_g"].min(),
                    max_value = penguins["body_mass_g"].max())
