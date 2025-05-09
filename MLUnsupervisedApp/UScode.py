d073fe90c32c8e7755a8c67359175cfe065aa4b0
Natalie <nataliebock19@gmail.com>
2025-05-08 21:21:56 -0400

    Update UScode.py

--- MLUnsupervisedApp/UScode.py
+++ MLUnsupervisedApp/UScode.py
@@ -79,9 +79,6 @@ if df is not None:
     if features:
         X = df[features] #defining X 
         y = None #since no target column for unsupervised learning
-        #making sure target_names works for any dataset  and can work with different variables / not just when the value is 0 or 1 
-        if y is not None:
-            target_names = [str(name) for name in y.unique()]
 
     #Model options 
     #providing brief descriptions of the 3 models offered
@@ -113,30 +110,30 @@ if df is not None:
             k = st.slider('Number of clusters (k)', 1, 10, 3) #set k to some number of clusters chosen on slider
             kmeans = KMeans(n_clusters = k) #number of clusters = whatever number chosen on the slider 
             kmeans.fit_predict(X_std) #finds the centroids and then clusters
-            clusters = kmeans.fit_predict(X_std)
+            clusters = kmeans.fit_predict(X_std) #defining clusters with kmeans
 #visualizing the model
             st.subheader("Visualizing the Results:") 
             st.markdown("- The scatterplot below shows how the model has clustered the data. Since the data has a large number of dimensions, PCA is used in order to reduce to 2D and allow for plotting.")
             st.markdown("- ***Reminder:*** PCA, prinipal component analysis, is a dimension reduction technique so it simplifies the features, in this case down to 2 to allow for visualization.")
             st.markdown("- The axes show the two dimensions, with the x-axis representing the first component and the y-axis showing the second component.")
             if X_std.shape[1] < 2: #making sure that the user has selected enough features to do pca, so program an error if less than 2 selected
-                st.error("Please select a minimum of 2 features above to perform PCA.")
+                st.error("Please select a minimum of 2 features above to perform PCA.") #creating an error message to ensure that users select more than 2 features for PCA to graph
             else:
                 pca = PCA(n_components=2)# = 2 so can visualize in 2D, easier to see.
-                X_pca = pca.fit_transform(X_std)
+                X_pca = pca.fit_transform(X_std) #fitting X_PCA 
 
             # Create a scatter plot of the PCA-transformed data, colored by KMeans cluster labels
-            plt.figure(figsize=(8, 6))
+            plt.figure(figsize=(8, 6)) #setting the size of the figure
             color_choices = ['red', 'blue', 'green', 'yellow', 'pink', 'orange', 'purple', 'black', 'cyan', 'magenta'] #providing color options so that no matter how many clusters the user selects, there is a color. Since allow to pick up to 10 clusters, there are 10 colors
-            for k in np.unique(clusters):
-                plt.scatter(X_pca[clusters == k, 0], X_pca[clusters == k, 1],
-                    c=color_choices[k % len(color_choices)], alpha=0.7, edgecolor='k', s=60, label=f'Cluster {k}')
-            plt.xlabel('Principal Component 1')
-            plt.ylabel('Principal Component 2')
-            plt.title('KMeans Clustering: 2D PCA Projection')
-            plt.legend(loc='best')
+            for k in np.unique(clusters): 
+                plt.scatter(X_pca[clusters == k, 0], X_pca[clusters == k, 1], #creating the scaterplot for the clustes
+                    c=color_choices[k % len(color_choices)], alpha=0.7, edgecolor='k', s=60, label=f'Cluster {k}') #applying the color choices, and using alpha to make points slightly transparent
+            plt.xlabel('Principal Component 1') #labeling the x axis
+            plt.ylabel('Principal Component 2') #labeling the y axis 
+            plt.title('KMeans Clustering: 2D PCA Projection') #title of the graphic 
+            plt.legend(loc='best') #setting the legend so that matlab picks where is best to place based on cluster locations 
             plt.grid(True)
-            st.pyplot(plt)
+            st.pyplot(plt) #so that streamlit plots the graph
 
 #elbow vs silhouette 
             st.subheader("Finding the Optimal Number of Clusters:") 
@@ -146,35 +143,35 @@ if df is not None:
             st.markdown("**NOTE:** While the Silhouette score is less subjective, there is no perfect metric, instead multiple metrics should be considered. Especially since unsupervised learning is part of exploratory data analysis, it is important to test different options and see which is best.")
             el_sil = st.radio("**Select An Evaluation Method for Determining the Optimal Number of Clusters**", ["Silhouette Score", "Elbow Method"]) #giving users the option to pick between two methods for evaluation
             if el_sil == "Silhouette Score": #if silhouette, 
-                ks = range(2,11)
-                wcss = []
+                ks = range(2,11) #setting the range of K's shown on the graph. Since the options are 2-10, showing 2-10
+                wcss = [] #for within cluster sum of squares for elbow method
                 silhouette_scores = []
                 for k in ks: # Silhouette scores for each k
                     km = KMeans(k) #fit kmeans model, will loop through doing k = 2,3,4,5,etc
                     km.fit(X_std) #just fitting the model ,then can take inertia attribute
-                    wcss.append(km.inertia_)
+                    wcss.append(km.inertia_) #inertia: sum of squared distances within clusters
                     labels = km.labels_ #grab the labels
                     silhouette_scores.append(silhouette_score(X_std, labels)) #looking at outside of cluster score for each point and labels, will grab scores
 #plotting the sil score
-                plt.figure(figsize=(8,6))
-                plt.plot(ks, silhouette_scores, marker='o', color='maroon')
-                plt.xlabel('Number of clusters (k)')
-                plt.ylabel('Silhouette Score')
-                plt.title('Silhouette Score for Optimal k')
-                plt.grid(True)
-                plt.tight_layout()
-                st.pyplot(plt)
+                plt.figure(figsize=(8,6)) #setting the size of the figure
+                plt.plot(ks, silhouette_scores, marker='o', color='maroon') #setting the color and creating the plot
+                plt.xlabel('Number of clusters (k)') #x axis label
+                plt.ylabel('Silhouette Score') #y axis label
+                plt.title('Silhouette Score for Optimal k') #plot title
+                plt.grid(True) #creating a grid 
+                plt.tight_layout() 
+                st.pyplot(plt) #showing the plot in streamlit
 #creating the elbow method plot 
             else: 
-                ks = range(2,11) #looking at k = all these numbers,  2 - 10
-                wcss = [] #so wcss reflects the data 
+                ks = range(2,11) #looking at k = all these numbers since the options are 2-10, showing 2-10
+                wcss = [] #for wcss
                 for k in ks:
-                    km = KMeans(k)
-                    km.fit(X_std)
+                    km = KMeans(k) #defining kmeans 
+                    km.fit(X_std) #fitting kmeans
                     wcss.append(km.inertia_)  #inertia: sum of squared distances within clusters
                     labels = km.labels_ #adding labels from kmeans 
-                plt.figure(figsize=(8,6))
-                plt.plot(ks, wcss, marker='o', color = 'navy')
+                plt.figure(figsize=(8,6)) #setting the size of the figure, same as Sil Score so easy to compare
+                plt.plot(ks, wcss, marker='o', color = 'navy') #setting the color and running the plot
                 plt.xlabel('Number of clusters (k)') #xaxis
                 plt.ylabel('Within-Cluster Sum of Squares (WCSS)') #yaxis
                 plt.title('Elbow Method for Optimal k') #title 
@@ -184,13 +181,13 @@ if df is not None:
 ##Principal Component Reduction
         elif mlselect == "principal component analysis": #if principal component analysis selected 
             st.markdown("You've selected principal component analysis, below you can adjust the number of components, decide whether to scale the data, and view the explained variance.") #what will appear when selecting pca
-            st.subheader("Components")
+            st.subheader("Components") #explaining what components do for PCA 
             st.markdown("Below you can select the number of components to use in the PCA, having a smaller number of components means that some information will be lost. **The number of components cannot be greater than the number of features (columns) chosen for use in the analysis.**")
             com_choice = st.slider('**Number of Components**', 1, 10, 3) #allowing user to pick number of components 
             st.subheader("Scaling") #giving users the the option to scale data and providing explanations for what scaling does
-            st.markdown("- Using unscaled data can make it difficult to compare features measured in different units. By scaling the data, it becomes easier to draw comparisons across the different features. For PCA, scaling is also important because it ensures that features on a larger numeric scale do not skew the impact and variance.")
+            st.markdown("- Using unscaled data can make it difficult to compare features measured in different units. By scaling the data, it becomes easier to draw comparisons across the different features. For PCA, scaling is also important because it ensures that features on a larger numeric scale do not skew the impact and variance.") #explaining scaled vs unscaled data
             st.markdown("- If the features are already in the same units, scaling may be unnecessary. It is important to consider what is necessary for your data, and your objectives.")
-            st.markdown("**NOTE:** PCA is sensitive to the scale of features, it is ***highly recommended*** to scale the data.")
+            st.markdown("**NOTE:** PCA is sensitive to the scale of features, it is ***highly recommended*** to scale the data.") #encouraging scaling! 
             app_scale = st.radio("**Select whether you would like to scale the model**", ["Unscaled", "Scaled"]) #giving users the option to pick between scaled and unscaled through radio prompt with a circle for each option
             if app_scale == "Scaled": #if scaled, apply scaler 
                 scaler = StandardScaler() #using standardscaler    
@@ -199,40 +196,38 @@ if df is not None:
                 X_std = X.values #if unscaled, will define X_std as the unscaled x values so still works with rest of code 
             #kmeans.fit_predict(X_std) #finds the centroids and then clusters
             #clusters = kmeans.fit_predict(X_std)
-            pca = PCA(n_components= com_choice)
-            X_pca = pca.fit_transform(X_std)
+            pca = PCA(n_components= com_choice) #having the number of components for PCA reflect user choice from the slider defined as com_choice 
+            X_pca = pca.fit_transform(X_std) #fitting the model 
 #Explained variance combined plot
-            st.subheader("Explained Variance")
+            st.subheader("Explained Variance") #explaining what the plot shows and what explained variance is 
             st.markdown("- The explained variance plot below shows a visualization of the cumulative variance to help the user determine the best number of components for the selected data set, a helpful insight for knowing how many components to use in future analysis.")
-            st.markdown("- **The green line** in the graph below uses the elbow method to determine the ideal number of components as cumulative variance explained. The elbow method evaluates by plotting the within-cluster sum of squares. In this case, the best number of clusters is identified by the ***elbow point***, where the decrease in WCSS flattens, or diminishes.")
-            st.markdown("- **The bars** below shows the individual variance explained, showing the variance that each separate component is able to explain. This is helpful for determining which components to use for further analysis.")
-            fig, ax1 = plt.subplots(figsize=(8, 6))
+            st.markdown("- **The green line** in the graph below uses the elbow method to determine the ideal number of components as ***cumulative variance*** explained. The elbow method evaluates by plotting the within-cluster sum of squares. In this case, the best number of clusters is identified by the ***elbow point***, where the decrease in WCSS flattens, or diminishes.")
+            st.markdown("- **The bars** below shows the ***individual variance*** explained, showing the variance that each separate component is able to explain. This is helpful for determining which components to use for further analysis.")
+            fig, ax1 = plt.subplots(figsize=(8, 6)) #setting the figure size 
             pca_sc = PCA(n_components=com_choice).fit(X_std) #making it so the number of components selected by user is the number reflected in PCA visual
             cumulative_variance = np.cumsum(pca_sc.explained_variance_ratio_) #calculating cumulative variance
             explained = pca_sc.explained_variance_ratio_ * 100  # variance for each component as a percentage 
-            components = np.arange(1, len(explained) + 1)
-            cumulative = np.cumsum(explained)
-            bar_color = 'steelblue'
-            ax1.bar(components, explained, color=bar_color, alpha=0.8, label='Individual Variance')
-            ax1.set_xlabel('Principal Component')
-            ax1.set_ylabel('Individual Variance Explained (%)', color=bar_color)
-            ax1.tick_params(axis='y', labelcolor=bar_color)
+            components = np.arange(1, len(explained) + 1) #starting number of components at 1 and ending at length chosen in explained percentage 
+            cumulative = np.cumsum(explained) #the cumulative sum for the number defined in explained
+            bar_color = 'purple' #setting the color
+            ax1.bar(components, explained, color=bar_color, alpha=0.8, label='Individual Variance')  #creating the bars for individual variance 
+            ax1.set_ylabel('Individual Variance Explained (%)', color=bar_color) #labeling the individual variance bar
+            ax1.tick_params(axis='y', labelcolor=bar_color) 
             ax1.set_xticks(components)
-            ax1.set_xticklabels([f"PC{i}" for i in components])
             for i, v in enumerate(explained):
                 ax1.text(components[i], v + 1, f"{v:.1f}%", ha='center', va='bottom', fontsize=10, color='black') #adding percentages to each bar
             ax2 = ax1.twinx()
-            line_color = 'green'
+            line_color = 'green' #defining line color 
             ax2.plot(components, cumulative, color=line_color, marker='o', label='Cumulative Variance') #second y axis for cumulative variance 
-            ax2.set_ylabel('Cumulative Variance Explained (%)', color=line_color)
+            ax2.set_ylabel('Cumulative Variance Explained (%)', color=line_color) #setting y axis 
             ax2.tick_params(axis='y', labelcolor=line_color)
-            ax2.set_ylim(0, 100)
+            ax2.set_ylim(0, 100) #limiting y to 100 since shown as percentages out of 100 
             ax1.grid(False) #removing grid lines
             ax2.grid(False) #removing grid lines
             lines1, labels1 = ax1.get_legend_handles_labels() # Combined legends
             lines2, labels2 = ax2.get_legend_handles_labels()# Combined legends
-            ax1.legend(lines1 + lines2, labels1 + labels2, loc='center right', bbox_to_anchor=(0.85, 0.5))
-            plt.title('PCA: Variance Explained', pad=20)
+            ax1.legend(lines1 + lines2, labels1 + labels2, loc='best') #setting the legend to best location found, 
+            plt.title('PCA: Variance Explained') #title for graph
             plt.tight_layout()
             st.pyplot(plt)
 # Show Explained Variance Ratios numerically 
